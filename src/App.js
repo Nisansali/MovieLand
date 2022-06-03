0d639350a499f1150fdeffc463d0b22227f63abc
Nisansali Nikapotha <44612011+Nisansali@users.noreply.github.com>
2022-06-03 13:20:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
     return(
        <div className="app">
-            <h1>MovieLand</h1>
+            <h1>MovieLanddsfsdfsdfs</h1>
 
             <div className="search">
                 <input 
