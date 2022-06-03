72f4b5fbb448d90c7b6b2b1ec53a754478bb3f4c
Nisansali Nikapotha <44612011+Nisansali@users.noreply.github.com>
2022-06-03 13:46:50 +0530

    update 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState([])
 
     const searchMovies = async (title) =>{
-        const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+        const response = await fetch(`https://www.omdbapi.com/?apikey=d5cb8dd8&s=spiderman`);
         const data = await response.json()
         setMovies(data.Search)
     }
